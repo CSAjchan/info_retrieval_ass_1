6649a3292ab6c084c5e8ca505bd919ba3e381143
Ajchan Mamedov <mamedova@tcd.ie>
2021-09-29 23:56:18 +0100

    LCA working on Java

--- LCA/src/LCA.java
+++ LCA/src/LCA.java
@@ -1,19 +1,28 @@
 public class LCA {
-
     public static void main(String[] args)
     {
         //create binary tree with its root
-        BinaryTree tree = new BinaryTree(1);
+        BinaryTree tree = new BinaryTree();
 
-        tree.root.left = new Node(2);
-        tree.root.right = new Node(3);
+        /* Let us create following BST
+              50
+           /     \
+          30      70
+         /  \    /  \
+       20   40  60   80 */
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.insert(80);
 
-        tree.root.left.left = new Node(4);
+        System.out.println("LCA(20, 40): " + tree.findLCA(20,40));
+        System.out.println("LCA(80, 20): " + tree.findLCA(80,20));
+        System.out.println("LCA(30, 80): " + tree.findLCA(30,80));
+        System.out.println("LCA(60, 80): " + tree.findLCA(60,80));
 
-        /*System.out.println(tree.root.key);
-        System.out.println(tree.root.left.key);
-        System.out.println(tree.root.right.key);
-        System.out.println(tree.root.left.left.key);*/
     }
 
 }
